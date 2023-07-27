640333d60eef404e025d74d5ca774c83360f3325
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-07-27 18:29:03 +0530

    copy over implementation from LowRankApprox

---
+++ ext/LowRankMatricesFillArraysExt.jl
@@ -0,0 +1,21 @@
+module LowRankMatricesFillArraysExt
+
+using LowRankMatrices
+using FillArrays
+using FillArrays: AbstractFill
+
+LowRankMatrix{T}(Z::Zeros, r::Int=0) where {T<:Number} =
+    LowRankMatrix(zeros(T,size(Z,1),r), zeros(T,size(Z,2),r))
+LowRankMatrix{T}(Z::Zeros, r::Int=0) where {T} =
+    LowRankMatrix(zeros(T,size(Z,1),r), zeros(T,size(Z,2),r))
+
+LowRankMatrix(Z::Zeros, r::Int=0) = LowRankMatrix{eltype(Z)}(Z, r)
+function LowRankMatrix{T}(F::AbstractFill) where T
+    v = T(FillArrays.getindex_value(F))
+    m,n = size(F)
+    LowRankMatrix(fill(v,m,1), fill(one(T),n,1))
+end
+LowRankMatrix(F::AbstractFill{T}) where T = LowRankMatrix{T}(F)
+
+
+end
--- src/LowRankMatrices.jl
+++ src/LowRankMatrices.jl
@@ -1,5 +1,18 @@
 module LowRankMatrices
 
-# Write your package code here.
+import Base: similar, convert, promote_rule, size, fill!, getindex,
+				*, +, -, \, /,
+				Matrix, copy, copyto!
+
+using LinearAlgebra
+import LinearAlgebra: rank, transpose, adjoint, mul!
+
+export LowRankMatrix
+
+include("lowrankmatrix.jl")
+
+if !isdefined(Base, :get_extension)
+    include("../ext/LowRankMatricesFillArraysExt.jl")
+end
 
 end
